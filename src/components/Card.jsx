97442f435c0969153da5d5b859a43c6d55ba5c61
Sowdarjya <kolaysowdarjya@gmail.com>
2024-10-02 08:01:45 +0530

    Added transactions component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ title, buttonText, onClick, amount, showBtn }) => {
   return (
-    <div className="m-3 w-1/3">
+    <div className="m-3 w-3/4 md:1/3">
       <div className="w-full bg-[#d3d9d4] p-4 rounded-lg shadow-xl shadow-[#212a31] h-[170px]">
         <h4 className="text-2xl mb-2">{title}</h4>
         <p className="text-3xl mb-2 font-bold">$ {amount}</p>
--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -6,6 +6,14 @@ const Transactions = () => {
       <h2 className="bg-[#212a31] text-[#d3d9d4] p-2 rounded-lg text-center font-semibold text-2xl">
         History
       </h2>
+      <ul>
+        <li className="flex items-center justify-around bg-[#d3d9d4] mt-2 p-2 rounded-lg shadow-2xl">
+          <p className="text-[#212a31] font-semibold">Date</p>
+          <p className="text-[#212a31] font-semibold">Description</p>
+          <p className="text-[#212a31] font-semibold">Amount</p>
+          <p className="text-[#212a31] font-semibold">Delete</p>
+        </li>
+      </ul>
     </div>
   );
 };
